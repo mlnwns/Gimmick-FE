ff258e949f3064c7ce083ecdedcc166edd85a428
jiucchu <128274035+jiucchu@users.noreply.github.com>
2024-11-15 16:00:24 +0900

    refactor: 폴더-생성-모달-페이지-styled-component-적용 (#12)

--- src/Pages/CreateSelectModal.jsx
+++ src/Pages/CreateSelectModal.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
-import {Pressable, StyleSheet, View, Modal, Text} from 'react-native';
+import {Modal} from 'react-native';
+import styled from 'styled-components/native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import CloseButton from '../Components/CloseButton';
 import CreateButton from '../Components/CreateButton';
@@ -8,67 +9,59 @@ const CreateSelectModal = () => {
   const [isModalVisible, setIsModalVisible] = useState(true);
 
   return (
-    <Modal
-      animationType="slide"
-      visible={isModalVisible}
-      transparent={true}
-      style={styles.modal}>
-      <View style={styles.modalView}>
-        <View style={styles.title}>
-          <CloseButton style={styles.closeButton} />
-          <Text style={styles.text}>새로 만들기</Text>
-        </View>
-        <View style={styles.buttons}>
+    <Modal animationType="slide" visible={isModalVisible} transparent={true}>
+      <ModalView>
+        <Title>
+          <StyledCloseButton />
+          <TitleText>새로 만들기</TitleText>
+        </Title>
+        <Buttons>
           <CreateButton
-            text={'폴더'}
+            text="폴더"
             icon={<Icon name="folder" size={25} color="gray" />}
-            style={styles.button}
           />
           <CreateButton
-            text={'타이머'}
+            text="타이머"
             icon={<Icon name="timer" size={25} color="gray" />}
-            style={styles.button}
           />
-        </View>
-      </View>
+        </Buttons>
+      </ModalView>
     </Modal>
   );
 };
 
-const styles = StyleSheet.create({
-  modal: {},
-  modalView: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    backgroundColor: 'white',
-  },
-  title: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderBottomWidth: 1,
-    borderColor: 'gray',
-    paddingVertical: 15,
-  },
-  text: {
-    textAlign: 'center',
-    fontSize: '18',
-    fontWeight: 500,
-  },
-  closeButton: {
-    position: 'absolute',
-    left: -10,
-  },
-  buttons: {
-    justifyContent: 'center',
-    flexDirection: 'row',
-    marginTop: 20,
-    marginBottom: 40,
-  },
-  button: {
-    marginHorizontal: 15,
-  },
-});
+const ModalView = styled.View`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: white;
+`;
+
+const Title = styled.View`
+  align-items: center;
+  justify-content: center;
+  border-bottom-width: 1px;
+  border-color: gray;
+  padding: 15px 0;
+`;
+
+const TitleText = styled.Text`
+  text-align: center;
+  font-size: 18px;
+  font-weight: 500;
+`;
+
+const StyledCloseButton = styled(CloseButton)`
+  position: absolute;
+  left: -10px;
+`;
+
+const Buttons = styled.View`
+  justify-content: center;
+  flex-direction: row;
+  margin: 20px 0 40px;
+  gap: 20px;
+`;
 
 export default CreateSelectModal;
