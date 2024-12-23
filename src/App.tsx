ae4d2950f78ae7e44e96d434157227e924dcc7e1
mlnwns <mlnwns@naver.com>
2024-12-23 19:48:04 +0900

    feat: 익명 함수 전달 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,49 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled from 'styled-components/native';
 import {scale} from 'react-native-size-matters';
 import MainPage from './pages/MainPage';
 import DetailPage from './pages/DetailPage';
 import {Platform} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {CustomConfig} from '../node_modules/react-native-reanimated/lib/typescript/layoutReanimation/web/config.d';
-import FolderCreatePage from './pages/FolderCreatePage';
-import TimerCreatePage from './pages/TimerCreatePage';
 
 const Stack = createNativeStackNavigator();
 
-const customStackNavigatorOptions: StackNavigationOptions = {
+const customStackNavigatorOptions = {
   headerShown: false,
   contentStyle: {backgroundColor: '#fff'},
 };
 
 function App(): React.JSX.Element {
   return (
-    // <BaseLayout>
     <NavigationContainer>
       <Stack.Navigator screenOptions={customStackNavigatorOptions}>
         <Stack.Screen
           name="Main"
-          component={() => (
-            <BaseLayout>
-              <MainPage />
-            </BaseLayout>
-          )}
+          component={MainWithLayout}
           options={{title: 'Main Page'}}
         />
         <Stack.Screen
           name="Detail"
-          component={() => (
-            <BaseLayout>
-              <DetailPage />
-            </BaseLayout>
-          )}
+          component={DetailWithLayout}
           options={{title: 'Detail Page'}}
         />
       </Stack.Navigator>
     </NavigationContainer>
-    // </BaseLayout>
   );
 }
+
+const MainWithLayout = () => (
+  <BaseLayout>
+    <MainPage />
+  </BaseLayout>
+);
+
+const DetailWithLayout = () => (
+  <BaseLayout>
+    <DetailPage />
+  </BaseLayout>
+);
 
 export default App;
 
